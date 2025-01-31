fbde1fe116fe1e9593faab0048842a3edd8a62e3
Javier Troya <javiertroya@gmail.com>
2025-01-31 15:00:07 +0100

    wp-die

--- src/app/Hooks/Auth/Block.php
+++ src/app/Hooks/Auth/Block.php
@@ -65,9 +65,11 @@ class Block {
             $context['email'] = isset($_GET['email']) ? sanitize_user($_GET['email']) : null;
             $context['first_time'] = isset($_GET['first_time']) ? 'yes' : null;
 
+            wp_die(print_r($context, 1));
+
             if(!$context['key'] && !$context['email']) {
-//                wp_redirect(wp_lostpassword_url());
-//                exit;
+                wp_redirect(wp_lostpassword_url());
+                exit;
             }
         }
     }
